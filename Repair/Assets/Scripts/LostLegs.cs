2494029f98aed102b773594ec7b44b35fb667a96
tracymeng2000 <tracymeng2000@gmail.com>
2020-02-01 22:05:15 -0500

    Added Seesaw Boundary

---
+++ Repair/Assets/Scripts/LostLegs.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class LostLegs : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == Constants.PLAYER)
+        {
+            Destroy(gameObject);
+            Destroy(GameObject.FindGameObjectWithTag(Constants.SEESAW_BOUNDARY).gameObject);
+        }
+    }
+}
